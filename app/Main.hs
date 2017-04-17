2ce5ee1de6dd5de7fd0cf258ad171221a3c13502
ronan <ronan_mccabe@protonmail.com>
2017-04-17 19:20:27 +0300

    Hit json placeholder.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
-module Main where
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
 import Lib
 
 main :: IO ()
-main = get "http://ronanmccabe.me/ping"
+main = go
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,33 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+
 module Lib
-    ( get
+    ( go
     ) where
 
-import Network.HTTP.Conduit
+import Data.Aeson
+import Data.Text
+import qualified Data.ByteString.Lazy as B
+import Network.HTTP.Conduit (simpleHttp)
+import GHC.Generics
 
-get :: String -> IO ()
-get url = simpleHttp url >>= print
+data Post =
+  Post { id     :: Int
+       , userId :: Int
+       , title  :: Text
+       , body   :: Text
+         } deriving (Show,Generic)
+
+instance FromJSON Post
+instance ToJSON Post
+
+jsonURL :: String
+jsonURL = "https://jsonplaceholder.typicode.com/posts/1"
+
+getJSON :: IO B.ByteString
+getJSON = simpleHttp jsonURL
+
+go = do
+ d <- (eitherDecode <$> getJSON) :: IO (Either String Post)
+ case d of
+  Left err -> putStrLn err
+  Right ps -> print ps
